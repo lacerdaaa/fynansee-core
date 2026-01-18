1c477d89c6d8d1a1c0878d4d55449380c0a6bbda
lacerdaaa <edulacerdaaa@gmail.com>
2026-01-17 22:38:11 -0300

    feat(migrations-config): add typeorm data source

---
+++ src/data-source.ts
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+import { DataSource } from 'typeorm';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST ?? 'localhost',
+  port: Number(process.env.DB_PORT ?? 5432),
+  username: process.env.DB_USER ?? 'postgres',
+  password: process.env.DB_PASSWORD ?? 'postgres',
+  database: process.env.DB_NAME ?? 'fynansee',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/migrations/*{.ts,.js}'],
+  synchronize: false,
+});
+
+export default AppDataSource;
