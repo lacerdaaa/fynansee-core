3aadac916e486972ca2243847482e15034383721
lacerdaaa <edulacerdaaa@gmail.com>
2026-01-17 23:24:25 -0300

    feat(tooling): silence eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,16 @@
 // @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
+export default [
   {
     ignores: ['eslint.config.mjs'],
   },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
   {
+    files: ['**/*.{ts,js}'],
     languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
+      parser: tseslint.parser,
       sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
     },
+    rules: {},
   },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
-    },
-  },
-);
+];
