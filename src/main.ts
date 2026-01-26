4b51ea51bdf301c4de70b75a2cfda7bb0ac55035
lacerdaaa <edulacerdaaa@gmail.com>
2026-01-25 21:23:29 -0300

    feat(api): enable cors for local apps

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:4200', 'http://localhost:4201'],
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
